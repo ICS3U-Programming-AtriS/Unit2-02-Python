441a3e6870df5b754e87e890a12d9e019136a1fe
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-03-02 04:25:18 +0000

    Added circ_cicle_tau.py and constants.py

---
+++ circ_circle_tau.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: March 1, 2025
+# Program that calculates the circumference of a circle,
+# using tau and user-defined radius
+import constants
+
+
+def main():
+    # Get the Radius from the user
+    radius = float(input("Enter the radius (cm): "))
+
+    # Calculate the circumference
+    circumference = constants.TAU * radius
+
+    # Display the circumference to the user
+    print(f"The circumference is {circumference:.2f}cm{constants.SUP_2}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: March 1, 2025
+# This module contains constants
+
+TAU = 6.28
+SUP_2 = "\u00b2"
